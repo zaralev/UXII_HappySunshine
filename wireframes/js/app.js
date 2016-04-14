846f7f52ff48f712b56bb96e381d33ed0c5bb95b
ZARA <ZLEVENTHAL@GMAIL.COM>
2016-04-13 20:41:17 -0400

    added enabled and disabled buttons

--- wireframes/js/app.js
+++ wireframes/js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation()
+
+$(document).ready(function(){
+	$(".next").hide();
+	$("input:radio").change(function () {$(".next").show();
+  $(".FORMPLZ").hide();
+
+
+  $(".next").click(function(){
+    $(".next").hide();
+    $(".FORMPLZ").show();
+  });
+
+});
+
+});
