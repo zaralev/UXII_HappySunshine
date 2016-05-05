9af38b6589b147a44481e176b667e7a646486381
Torres <det52@n1-142-74.dhcp.drexel.edu>
2016-05-05 15:58:58 -0400

    local storage needs more work

--- js/lowfidelity/storage.js
+++ js/lowfidelity/storage.js
@@ -1,5 +1,5 @@
 // Create Accout Local Storage
-
+// NOT FINISHED NEEDS MORE WORK
 // this function sets variables and local storage
 function accountDetails() {
  // create variables to hold inputed values
@@ -8,6 +8,10 @@ function accountDetails() {
  // sets local storage to the variables that were set earlier
  localStorage.setItem("account-email", email.value);
  localStorage.setItem("account-password", pw.value);
+
+ if (email || pw == ""){
+   alert("Please Enter an email or password");
+ }
 }
 console.log(localStorage.getItem("account-email"));
 console.log(localStorage.getItem("account-password"));
