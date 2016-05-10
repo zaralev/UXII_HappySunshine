af2824d78dc5b094d999f607ec02d39f8ad434b5
ZARA <ZLEVENTHAL@GMAIL.COM>
2016-05-10 10:50:49 -0400

    added arrow on menu and rotate when clicked, fixed button, moved css to scss

---
+++ highfidelity/arrow-rotate.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.arrow').click(function() {
+      $(this).css("transform", "rotate(0deg)");
+      $(this).css("ms-transform", "rotate(0deg)");
+      $(this).css("webkit-transform", "rotate(0deg)");
+    })
+});
