250e0f22b6b267baf0336e4b3bc7116943cf9eed
Torres <det52@n1-142-43.dhcp.drexel.edu>
2016-05-10 15:24:33 -0400

    local storage for login/ create account functionality. Email and Password fields cannot be left blank or the user will not be able to proceed.

--- js/lowfidelity/storage.js
+++ js/lowfidelity/storage.js
@@ -1,5 +1,6 @@
 // Create Accout Local Storage
 // NOT FINISHED NEEDS MORE WORK
+
 // this function sets variables and local storage
 function accountDetails() {
  // create variables to hold inputed values
@@ -9,14 +10,22 @@ function accountDetails() {
  localStorage.setItem("account-email", email.value);
  localStorage.setItem("account-password", pw.value);
 
- if (email || pw == ""){
-   alert("Please Enter an email or password");
- }
-}
-console.log(localStorage.getItem("account-email"));
-console.log(localStorage.getItem("account-password"));
+ console.log(localStorage.getItem("account-email").length);
+ console.log(localStorage.getItem("account-password").length);
 
-// this function
+ console.log(localStorage.getItem("account-email"));
+ console.log(localStorage.getItem("account-password"));
+
+// If user does not enter anything, wont be allowed to go to home page
+ if (localStorage.getItem("account-email").length <= 0 || localStorage.getItem("account-password").length <= 0) {
+   alert("Please enter an Email or Password");
+ }else {
+   window.location.href="index.html";
+ }
+
+ }
+
+// this function runs on the clicking login on index.html page
 function loginValidation() {
  // getting the values entered in the login screen
  var enteredEmail = document.getElementById("email");
@@ -25,10 +34,15 @@ function loginValidation() {
  var userEmail = enteredEmail.value;
  var userPw = enteredPw.value;
 
-// if/else to determine if email and pw are equal and proceed to menu page
- if(userEmail == localStorage.getItem("account-email") && userPw == localStorage.getItem("account-password")){
+// if user does not enter anything on home page they will not be able to login in
+if (userEmail.length <=0 || userPw.length <= 0) {
+  alert("Please enter your email or password");
+  // password and email need to be the same length in order to proceed to menu page/rest of app
+}else if (userEmail.length == localStorage.getItem("account-email").length && userPw.length == localStorage.getItem("account-password").length) {
   window.location.href="menu.html";
- }else{
-  alert("Email or password not recognized. Please re-enter valid email or password or create an account");
- }
+}else{
+  console.log(userEmail);
 }
+
+}
+// localStorage.clear();
