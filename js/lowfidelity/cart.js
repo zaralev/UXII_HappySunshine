99a68a9506e2f9986c9ea800223de4eaabccb94a
Torres <det52@n1-140-103.dhcp.drexel.edu>
2016-05-13 15:55:41 -0400

    Local storage for menu, not finished needs more work

--- js/lowfidelity/cart.js
+++ js/lowfidelity/cart.js
@@ -1,28 +1,31 @@
-$(document).ready(function(){
-  var cart = {};
-  cart.foodForCart = [];
-
-  localStorage.setItem('cart', JSON.stringify(cart));
-
-  $('.add-to').click(function(){
-    var li = $(this).parent();
-    var span = $('.price');
-
-    var foodItems = {};
-    foodItems.name = $(li).attr("data-menu-item");
-    foodItems.price = $(span).attr("data-item-price");
-
-    console.log(foodItems);
-    addToCart(foodItems);
-  });
-});
-
-function addToCart(foodItems){
-  if (localStorage && localStorage.getItem('cart')) {
-    var cart = JSON.parse(localStorage.getItem('cart'));
-
-    cart.foodForCart.push(foodItems);
-
-    localStorage.setItem('cart', JSON.stringify(cart));
-  }
-}
+// $(document).ready(function(){
+//     var cart = [];
+//     localStorage.setItem("cart", JSON.stringify(cart));
+//
+//   $('.add-to').click(function(){
+//
+//     var li = $(this).parent();
+//     var food = {};
+//     food.name = $(li).attr('data-menu-item');
+//     food.price = $(li).attr('data-item-price');
+//
+//     cart.push(food);
+//
+//     localStorage.setItem("cart", JSON.stringify(cart));
+//     console.log(localStorage.getItem("cart"));
+//
+//     displayCart();
+//
+//     function displayCart(){
+//       console.log("** Display Cart **");
+//       var cartArray = localStorage.getItem("cart");
+//       console.log(cartArray.length);
+//       var output = "";
+//       for (var i in cartArray){
+//         output += "<li>" + cartArray[i].name+ " " + cartArray[i].price + "</li>"
+//       }
+//       $('.show-cart').html(output);
+//     }
+//
+//   });
+// });
