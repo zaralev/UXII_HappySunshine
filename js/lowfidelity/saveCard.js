f38cbe0dab09f785d13c3bd3a4ea151e839ac2bc
Torres <det52@n1-142-197.dhcp.drexel.edu>
2016-05-20 17:14:03 -0400

    working on saving credit card details

--- js/lowfidelity/saveCard.js
+++ js/lowfidelity/saveCard.js
@@ -1,3 +1,31 @@
-function saveCard(){
-  
-}
+// $(document).ready(function(){
+//   $(".success button save").click(function(){
+//     $("#card-details input").each(function(){
+//       card.push(this.value);
+//     });
+//   });
+// });
+//
+//
+// // ---------- Saving Credit Card Details
+//
+// var saveCard = (function(){
+//   var card = [];
+//
+//   function CardDetails(name, number, dateOne, dateTwo, SecCode){
+//     this.name = name;
+//     this.number = number;
+//     this.dateOne = dateOne;
+//     this.dateTwo = dateTwo;
+//     this.SecCode = SecCode;
+//   }
+//
+//   function saveCardDetails(){
+//     localStorage.setItem("saveCard", JSON.stringify(card));
+//   }
+//   function loadCardDetails(){
+//     card = JSON.parse(localStorage.getItem("saveCard"));
+//   }
+// 
+//   loadCardDetails();
+// });
