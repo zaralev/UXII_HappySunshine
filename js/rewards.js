d3be354ea3a552de9c693473c437c2eb943b7d81
ZARA <ZLEVENTHAL@GMAIL.COM>
2016-05-31 00:10:02 -0400

    Started to write localStorage function for rewards. However, it doesn't 'function'

---
+++ js/rewards.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // this doesn't work and I don't know why
+    var count;
+    $("#confirm-button").click(function() {
+        count++;
+        var rewardNum = parseInt(localStorage.getItem("clicks"));
+        var newCount = rewardNum + count;
+        localStorage.setItem("clicks", newCount);
+        console.log(localStorage.getItem("clicks"));
+    });
+
+
+});
